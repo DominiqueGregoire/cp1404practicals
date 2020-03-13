1020db2c9478536bfeffa550e0058fdad521ee3e
Dominique Gregoire <dominique.gregoire@my.jcu.edu.au>
2020-03-13 20:40:11 +1000

    allow us to use the function with or without user input

--- broken_score.py
+++ broken_score.py
@@ -4,13 +4,18 @@ Broken program to determine score status.
 Once fixed this will allow a user to input
 a score and print out the result
 """
+import random
 
 
 def main():
     score = float(input("Enter score: "))
-
     result = get_user_score(score)
     print("{}\t {}".format(score, result))
+
+    random_score = random.randint(0, 100)
+
+    result = get_user_score(random_score)
+    print("{}\t {}".format(random_score, result))
 
 
 def get_user_score(score):
