ee0dd2a2c0974b4a256163bb11959448db803011
Dominique Gregoire <dominique.gregoire@my.jcu.edu.au>
2020-03-13 19:07:43 +1000

    refactor the broken score program to use functions.

--- broken_score.py
+++ broken_score.py
@@ -1,15 +1,27 @@
 """
 CP1404/CP5632 - Practical
-Broken program to determine score status
+Broken program to determine score status.
+Once fixed this will allow a user to input
+a score and print out the result
 """
 
-score = float(input("Enter score: "))
 
-if 90 <= score <= 100:
-    print("Excellent")
-elif 50 <= score < 90:
-    print("Passable")
-elif 50 > score >= 0:
-    print("Bad")
-else:
-    print("Invalid score")
+def main():
+    score = float(input("Enter score: "))
+
+    result = get_user_score(score)
+    print("{}\t {}".format(score, result))
+
+
+def get_user_score(score):
+    if 90 <= score <= 100:
+        return "Excellent"
+    elif 50 <= score < 90:
+        return "Passable"
+    elif 50 > score >= 0:
+        return "Bad"
+    else:
+        return "Invalid score"
+
+
+main()
