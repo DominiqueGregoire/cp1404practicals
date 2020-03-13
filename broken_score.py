58bdbb79573218741572a68ac1082140e51cdcfe
Dominique Gregoire <dominique.gregoire@my.jcu.edu.au>
2020-03-13 18:45:16 +1000

    Fix a broken program that determines the score status

---
+++ broken_score.py
@@ -0,0 +1,15 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+score = float(input("Enter score: "))
+
+if 90 <= score <= 100:
+    print("Excellent")
+elif 50 <= score < 90:
+    print("Passable")
+elif 50 > score >= 0:
+    print("Bad")
+else:
+    print("Invalid score")
