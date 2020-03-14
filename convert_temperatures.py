6948e4c09f9e36a1b1dc712150b134317cb4cd7f
Dominique Gregoire <dominique.gregoire@my.jcu.edu.au>
2020-03-15 09:52:51 +1000

    change the name of file from temperature_input.txt to temps_input.txt

---
+++ convert_temperatures.py
@@ -0,0 +1,7 @@
+"""Create a program that will read the temps_input_txt file as fahrenheit
+temperatures and convert them to Celsius.  The converted temperatures will
+be then stored in a file named temps_output.txt"""
+
+
+def main():
+    temps_
--- more_temperatures.py
+++ more_temperatures.py
@@ -3,15 +3,15 @@ import random
 
 
 def main():
-    temperature_input_file = open("temps_input.txt", 'w')
+    temps_input_file = open("temps_input.txt", 'w')
     random_temperature = random.uniform(-200.0, 200.0)
 
     for temperature in range(5):
         temperature = random_temperature
-        print(temperature, file=temperature_input_file)
+        print(temperature, file=temps_input_file)
         random_temperature = random.uniform(-200.0, 200.0)
 
-    temperature_input_file.close()
+    temps_input_file.close()
 
 
 main()
