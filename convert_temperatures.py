7c2de5fee8ded0d6e2934898e4d517c94409ccb0
Dominique Gregoire <dominique.gregoire@my.jcu.edu.au>
2020-03-15 11:06:15 +1000

    ask the user for the amount of temperatures to be stored in the temps_input.txt file

--- convert_temperatures.py
+++ convert_temperatures.py
@@ -4,4 +4,21 @@ be then stored in a file named temps_output.txt"""
 
 
 def main():
-    temps_
+    temps_input_file = open('temps_input.txt', 'r')
+    temps_output_file = open('temps_output.txt', 'w')
+
+    for line in temps_input_file:
+        line = temps_input_file.readline().strip('\n')
+        print(line)
+        fahrenheit = float(line)
+        print(fahrenheit + 1)
+#         celsius = convert_fahrenheit_to_celsius(temp)
+#         print(celsius)
+#
+#
+# def convert_fahrenheit_to_celsius(fahrenheit):
+#     celsius = 5 / 9 * (fahrenheit - 32)  # celsius = 5 / 9 * (fahrenheit - 32)
+#     return celsius
+
+
+main()
--- more_temperatures.py
+++ more_temperatures.py
@@ -1,12 +1,14 @@
 """create a temperature input file with at least 15 floats"""
 import random
 
+quantity = int(input('Enter quantity: '))
+
 
 def main():
     temps_input_file = open("temps_input.txt", 'w')
     random_temperature = random.uniform(-200.0, 200.0)
 
-    for temperature in range(5):
+    for temperature in range(quantity):
         temperature = random_temperature
         print(temperature, file=temps_input_file)
         random_temperature = random.uniform(-200.0, 200.0)
