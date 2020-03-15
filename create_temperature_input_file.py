7ae95c28bac1bdf74840034ae1aa81570a5347b8
Dominique Gregoire <dominique.gregoire@my.jcu.edu.au>
2020-03-15 13:01:51 +1000

    Refactor program and name of python file to make it more readable

--- create_temperature_input_file.py
+++ create_temperature_input_file.py
@@ -1,17 +1,15 @@
 """create a temperature input file with at least 15 floats"""
 import random
 
-quantity = int(input('Enter quantity: '))
+quantity = int(input('Enter quantity of inputs: '))
 
 
 def main():
     temps_input_file = open("temps_input.txt", 'w')
-    random_temperature = random.uniform(-200.0, 200.0)
 
     for temperature in range(quantity):
-        temperature = random_temperature
+        temperature = random.uniform(-200, 200)
         print(temperature, file=temps_input_file)
-        random_temperature = random.uniform(-200.0, 200.0)
 
     temps_input_file.close()
 
