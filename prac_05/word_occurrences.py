7b65282fdd6d78aaab5cbf3c2aed659e873e56d1
Dominique Gregoire <dominique.gregoire@my.jcu.edu.au>
2020-04-03 19:28:20 +1000

    format the display and align the outputs so the numbers are in one nice column

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -13,13 +13,12 @@ for word in word_string.split():     # split the string into individual words
 
 # sort and print the unique words in the dictionary
 unique_words = list(word_occurrences_dict.keys())
-print(unique_words)
 unique_words.sort()
-print(unique_words)
 
+#  use the length of the longest word to format the display
+word_lengths = [len(word) for word in unique_words]
+longest_word = max(word_lengths)
 
-#    retrieves and displays formatted word/occurrence pair
+#  retrieves and displays formatted and aligned word/occurrence pair
 for word in unique_words:
-    print("{} : {}".format(word, word_occurrences_dict[word]))
-
-# print(word_occurrences_dict)
+    print("{:{}} : {}".format(word, longest_word, word_occurrences_dict[word]))
