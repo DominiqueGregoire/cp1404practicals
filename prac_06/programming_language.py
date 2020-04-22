fccbccd57a211e1541ec9cbb681ef599e5008513
Dominique Gregoire <dominique.gregoire@my.jcu.edu.au>
2020-04-23 09:21:38 +1000

    Reformat the is_dynamic function method by changing self.is_dynamic to self.typing and "dynamic" to "Dynamic" thereby solving recurrence problem.

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -7,12 +7,39 @@ For each language the following fields will be stored; typing(static/dynamic),
 
 class ProgrammingLanguage:
 
-    def __init__(self, typing, reflection, name='', year=0):
+    def __init__(self, name='', typing='', reflection='', year=0):
+        self.reflection = reflection
         self.name = name
         self.typing = typing
-        self.reflection = reflection
         self.year = year
 
+    def __str__(self):
+        return "{self.name}, {self.typing} Typing, Reflection={self.reflection}, First " \
+               "appeared in {self.year}".format(self=self)
+
     def is_dynamic(self):
-        if self.typing == 'dynamic':
-            return True
+        return self.typing == 'Dynamic'
+
+
+def run_tests():
+    lang1 = ProgrammingLanguage()
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+    print(lang1)
+    print(python)
+
+    """To ensure that the is_dynamic function returns get what is expected"""
+    print('{} is_dynamic - Expected True. Got {}'.format(python.name, python.is_dynamic()))
+    print('{} is_dynamic - Expected False. Got {}'.format(visual_basic.name, visual_basic.is_dynamic()))
+
+    languages = [ruby, python, visual_basic]
+    print("The dynamically typed languages are:")
+    for language in languages:
+        # print(language)
+        if language.is_dynamic():
+            print(language.name)
+
+
+if __name__ == '__main__':
+    run_tests()
