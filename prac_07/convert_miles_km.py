6fc39d1da1c9a182a201cb6aabeda6660f324124
Dominique Gregoire <dominique.gregoire@my.jcu.edu.au>
2020-04-30 12:57:55 +1000

    extend the program by creating function to handle an incremental increase/decrease when the up/down button is pressed.

--- prac_07/convert_miles_km.py
+++ prac_07/convert_miles_km.py
@@ -5,6 +5,7 @@ A simple kivy program that will allow a user to convert Miles to Kilometers.
 from kivy.app import App
 from kivy.lang import Builder
 
+
 class MilesConverter(App):
     def build(self):
         self.title = "Convert Miles to Kilometers"
@@ -17,6 +18,16 @@ class MilesConverter(App):
         value = float(self.root.ids.user_input.text)
         result = value * miles_unit_conversion
         self.root.ids.output_label.text = str(result)
+
+    def handle_increment(self, increment):
+        """
+        increase/decrease the users input by the increment
+        :param increment:
+        :return:
+        """
+        result = int(self.root.ids.user_input.text) + increment
+        self.root.ids.user_input.text = str(result)
+        self.handle_conversion_to_km()
 
 
 MilesConverter().run()
