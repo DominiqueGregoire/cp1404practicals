4ca54bd0ae3df034f6830df82b106c473946d806
Dominique Gregoire <dominique.gregoire@my.jcu.edu.au>
2020-05-08 11:50:22 +1000

    Initialise a taxi simulator program

---
+++ prac_08/taxi_simulator.py
@@ -0,0 +1,30 @@
+"""CP1404 prac_08
+Taxi simulator program that includes a list of available taxis
+for the user to choose from. The user can choose how far to drive
+the taxi. The cost of the trip will be shown after each trip and added
+to their bill."""
+
+from prac_08.silver_service_taxi import SilverServiceTaxi
+from prac_08.taxi import Taxi
+
+
+def main():
+    current_taxi = None
+
+    taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    print('Lets Drive!')
+
+    menu_choice = input("q)uit, c)hoose taxi, d)rive\n>>> ").lower()
+    print(menu_choice)
+
+    while menu_choice != "q":
+        if menu_choice == "c":
+            pass
+        elif menu_choice == "d":
+            pass
+        else:
+            print("Invalid menu choice.")
+        menu_choice = input("q)uit, c)hoose taxi, d)rive\n>>> ").lower()
+
+
+main()
