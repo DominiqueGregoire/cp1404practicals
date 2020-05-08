a38a416bd985818e3a02cda6da4dc5032846ae73
Dominique Gregoire <dominique.gregoire@my.jcu.edu.au>
2020-05-08 11:30:46 +1000

    Update the get_fare method function in taxi to return a price rounded to the nearest 10c, and refactor the test programs.

--- prac_08/silver_service_taxi_tests.py
+++ prac_08/silver_service_taxi_tests.py
@@ -12,7 +12,7 @@ def run_tests():
     print("price per km should be 2.46 got {}".format(sstaxi1.price_per_km))
     print("Fanciness of 2, price per km should be 2.46 got {}".format(sstaxi1.price_per_km))
     print("flagfall should be 4.50 got {:.2f}".format(sstaxi1.flagfall))
-    print("fare should be 48.78 got {}".format(sstaxi1.get_fare()))
+    print("fare should be 48.78 got {:.2f}".format(sstaxi1.get_fare()))
 
 
 run_tests()
--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -6,12 +6,17 @@ from prac_08.taxi import Taxi
 def run_tests():
     """Test Taxi class"""
     taxi1 = Taxi("Prius 1", 100)
-    taxi1.drive(40)
+    print(taxi1)
+    taxi1.drive(4)
     print(taxi1.__str__())
+    print("cost of trip should be 4 * {} = 4.92 rounded up should be 4.90 "
+          "got {:.2f}".format(taxi1.price_per_km, taxi1.get_fare()))
     taxi1.start_fare()
     print(taxi1.__str__())
     taxi1.drive(100)
     print(taxi1.__str__())
+
+
 
 
 run_tests()
