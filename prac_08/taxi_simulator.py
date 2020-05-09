8c0d814086114b2f378ac35ea849d9437e05ab9f
Dominique Gregoire <dominique.gregoire@my.jcu.edu.au>
2020-05-09 11:23:35 +1000

    extend the taxi simulator by adding the drive menu choice

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -19,11 +19,24 @@ def main():
 
     while menu_choice != "q":
         if menu_choice == "c":
-            choose_a_taxi(taxis)
+            chosen_taxi = int(choose_a_taxi(taxis))
+            # print(chosen_taxi)  # to be commented out
+            current_taxi = taxis[chosen_taxi]
+            # print(current_taxi)
+
         elif menu_choice == "d":
-            pass
+            current_taxi.start_fare()
+            # print(current_taxi)
+            distance = float(input("Drive how far? "))
+            # print(distance)
+            current_taxi.drive(distance)
+            # print(current_taxi)
+            print("Your {} trip cost you ${:.2f}".format(current_taxi.name, current_taxi.get_fare()))
+
         else:
             print("Invalid menu choice.")
+
+            print("Bill to date: {}".format(bill_to_date()))  # need to put in bill to date
         menu_choice = input("q)uit, c)hoose taxi, d)rive\n>>> ").lower()
 
 
@@ -31,6 +44,11 @@ def choose_a_taxi(taxis):
     """Print a list of available taxis for the user to choose from"""
     for i, taxi in enumerate(taxis):
         print(i, "- {}".format(taxi))
+    return input("Choose taxi: ")
+
+
+def bill_to_date():
+    pass
 
 
 main()
