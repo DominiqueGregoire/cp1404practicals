66cb20cd751ffc1e8aca1096842f8fa9ac8e50aa
Dominique Gregoire <dominique.gregoire@my.jcu.edu.au>
2020-05-09 13:19:52 +1000

    refactor program by adding quit menu choice, changing choose_a_taxi function to display_available_taxis, and tidying up the program.

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -15,45 +15,37 @@ def main():
     print('Lets Drive!')
 
     menu_choice = input("q)uit, c)hoose taxi, d)rive\n>>> ").lower()
-    # print(menu_choice)
 
     while menu_choice != "q":
         if menu_choice == "c":
-            chosen_taxi = int(choose_a_taxi(taxis))
-            # print(chosen_taxi)  # to be commented out
+            display_available_taxis(taxis)
+            chosen_taxi = int(input("Choose taxi: "))
             current_taxi = taxis[chosen_taxi]
 
         elif menu_choice == "d":
             current_taxi.start_fare()
-            # print(current_taxi)
             distance = float(input("Drive how far? "))
-            # print(distance)
             current_taxi.drive(distance)
-            # print(current_taxi)
             print("Your {} trip cost you ${:.2f}".format(current_taxi.name, current_taxi.get_fare()))
             bill_to_date += current_taxi.get_fare()
-            # print("Bill to date: {}".format(bill_to_date))
 
         else:
             print("Invalid menu choice.")
 
-        print("Bill to date: {}".format(bill_to_date))
+        print("Bill to date: ${:.2f}".format(bill_to_date))
         menu_choice = input("q)uit, c)hoose taxi, d)rive\n>>> ").lower()
 
+    if menu_choice == "q":
+        print("Total trip cost: ${:.2f}".format(bill_to_date))
+        print("Taxis are now:")
+        display_available_taxis(taxis)
 
-def choose_a_taxi(taxis):
+
+def display_available_taxis(taxis):
     """Print a list of available taxis for the user to choose from"""
     print("Taxis available:")
     for i, taxi in enumerate(taxis):
         print(i, "- {}".format(taxi))
-    return input("Choose taxi: ")
-
-
-# def bill_to_date(current_taxi):
-#     total_fare = 0
-#     total_fare += current_taxi.get_fare()
-#     print("inside", total_fare)
-#     return total_fare
 
 
 main()
